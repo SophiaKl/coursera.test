66d2f2c95c7055acff48533b70ed347bac083e15
Sophia Klingler <sophiaklingler@gmail.com>
2020-05-11 13:01:29 +0200

    new

--- module4/script.js
+++ module4/script.js
@@ -33,7 +33,10 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
+helloSpeaker.helloSpeaker();
+byeSpeaker.byeSpeaker();
+
+(function (names) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
